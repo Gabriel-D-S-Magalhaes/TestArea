303a776c35477d97795fd24ac30262af043709ec
mac <gabrielsantosdsm@gmail.com>
2018-01-10 17:00:18 -0200

    Localizar fonte de som agr roda em background (usando Asynctask) a partir do onCreate(); Crie um método responsavel por virar o Sanbot na direção da fonte de som.
    Resumo do fluxo:
    App é aberto; Na criação da Activity, em background, é captado as fontes de sons e armazenado em uma variavel global que refere-se ao ângulo da origem do som; Quando o btn é apertado o mic é aberto e se ouvido "Robot" o Sanbot vira em direção a última fonte de som capturada, que TEORICAMENTE é a pessoa que deseja interagir com o robo; NOTE: Pode haver erros, e.g.: A pessoa diz "Robot", mas ao mesmo tempo houve um barulho, podendo assim ter sido capturado a origem do barulho ao inves da pessoa chamando "Robot".

--- app/src/main/java/vivacity/com/br/testarea/MainActivity.java
+++ app/src/main/java/vivacity/com/br/testarea/MainActivity.java
@@ -1,5 +1,6 @@
 package vivacity.com.br.testarea;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,12 +10,13 @@ import android.widget.Toast;
 import com.google.gson.GsonBuilder;
 import com.qihancloud.opensdk.base.TopBaseActivity;
 import com.qihancloud.opensdk.beans.FuncConstant;
+import com.qihancloud.opensdk.function.beans.speech.Grammar;
 import com.qihancloud.opensdk.function.beans.wheelmotion.RelativeAngleWheelMotion;
 import com.qihancloud.opensdk.function.unit.HardWareManager;
 import com.qihancloud.opensdk.function.unit.SpeechManager;
 import com.qihancloud.opensdk.function.unit.WheelMotionManager;
 import com.qihancloud.opensdk.function.unit.interfaces.hardware.VoiceLocateListener;
-import com.qihancloud.opensdk.function.unit.interfaces.speech.WakenListener;
+import com.qihancloud.opensdk.function.unit.interfaces.speech.RecognizeListener;
 
 import java.io.IOException;
 
@@ -29,20 +31,25 @@ public class MainActivity extends TopBaseActivity {
     private static final String TAG = "MainActivity";
     private static final int FULL_ANGLE = 360;
 
+    private int angleToTurn;
+
+    Listerning listerning = new Listerning();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Keep screen awake
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         httpMethods = new HTTPMethods();
 
         hardWareManager = (HardWareManager) getUnitManager(FuncConstant.HARDWARE_MANAGER);
         wheelMotionManager = (WheelMotionManager) getUnitManager(FuncConstant.WHEELMOTION_MANAGER);
         speechManager = (SpeechManager) getUnitManager(FuncConstant.SPEECH_MANAGER);
+
+        listerning.execute();
     }
 
     public void search(View view) {
@@ -69,12 +76,10 @@ public class MainActivity extends TopBaseActivity {
     }
 
     /**
-     * @param view
-     *
-     * Responsável por virar o Sanbot na direção da fonte de som.
+     * @param view Responsável por virar o Sanbot na direção da fonte de som.
      */
     public void wheelControl(View view) {
-        searchSoundSource();
+        heardSanbot();
     }
 
     public void searchSoundSource() {
@@ -91,28 +96,97 @@ public class MainActivity extends TopBaseActivity {
             public void voiceLocateResult(int i) {
 
                 // Info para o user
-                Toast.makeText(MainActivity.this, "Localizando fonte de som...", Toast.LENGTH_SHORT).show();
-
-                // Se o ângulo é até 180
-                if (i >= 0 && i <= 180) {
-
-                    //então viro normalmente (sentido horário), para á direita.
-                    wheelMotionManager.doRelativeAngleMotion(new RelativeAngleWheelMotion(
-                            RelativeAngleWheelMotion.TURN_RIGHT, 10, i));
-
-                } else if (i > 180 && i <= 360) {
-                    // se o ângulo vai de 181 a 360 graus, então (para não ficar demorado
-                    // virando no sentido horário), faço: 360 - i e viro á esquerda.
-                    wheelMotionManager.doRelativeAngleMotion(new RelativeAngleWheelMotion(
-                            RelativeAngleWheelMotion.TURN_LEFT, 10,
-                            FULL_ANGLE - i));
-                }
+                //Toast.makeText(MainActivity.this, "Localizando fonte de som... a " + i + "º", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Localizando fonte de som...");
+                angleToTurn = i;
             }
         });
+    }
+
+    public void heardSanbot() {
+
+        speechManager.doWakeUp();
+
+        speechManager.setOnSpeechListener(new RecognizeListener() {
+
+            @Override
+            public boolean onRecognizeResult(Grammar grammar) {
+
+                if (!grammar.getText().isEmpty() && grammar.getText().contains("Robot")) {
+
+                    turnSanbot(angleToTurn);
+                    Log.i(TAG, "Ouviu Robot a " + angleToTurn + "º");
+                    Toast.makeText(MainActivity.this, "Ouviu Robot a " + angleToTurn + "º", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                Log.i(TAG, "Sanbot understood: " + grammar.getText());
+                return false;
+            }
+
+            @Override
+            public void onRecognizeVolume(int i) {
+
+            }
+        });
+    }
+
+    public void turnSanbot(int angle) {
+
+        // Se o ângulo é até 180
+        if (angle >= 0 && angle <= 180) {
+
+            //então viro normalmente (sentido horário), para á direita.
+            wheelMotionManager.doRelativeAngleMotion(new RelativeAngleWheelMotion(
+                    RelativeAngleWheelMotion.TURN_RIGHT, 10, angle));
+
+        } else if (angle > 180 && angle <= 360) {
+
+            // se o ângulo vai de 181 a 360 graus, então (para não ficar demorado
+            // virando no sentido horário), faço: 360 - angle e viro á esquerda.
+            wheelMotionManager.doRelativeAngleMotion(new RelativeAngleWheelMotion(
+                    RelativeAngleWheelMotion.TURN_LEFT, 10,
+                    FULL_ANGLE - angle));
+        }
+
     }
 
     @Override
     protected void onMainServiceConnected() {
 
+    }
+
+
+    class Listerning extends AsyncTask<Void, Void, Void> {
+
+        private static final String TAG = "Listerning";
+
+        /**
+         * Override this method to perform a computation on a background thread. The
+         * specified parameters are the parameters passed to {@link #execute}
+         * by the caller of this task.
+         * <p>
+         * This method can call {@link #publishProgress} to publish updates
+         * on the UI thread.
+         *
+         * @param voids The parameters of the task.
+         * @return A result, defined by the subclass of this task.
+         * @see #onPreExecute()
+         * @see #onPostExecute
+         * @see #publishProgress
+         */
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Log.i(TAG, "doInBackground running");
+            searchSoundSource();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.i(TAG, "onPostExecute running");
+            //turnSanbot(integer);
+        }
     }
 }
