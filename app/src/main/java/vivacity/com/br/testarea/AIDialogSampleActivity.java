762dc73ec7720a19bf037978adb5eea99cd149ba
mac <gabrielsantosdsm@gmail.com>
2018-01-24 17:22:44 -0200

    Add modificações nos arquivos .proto Add SpeechActivity.java Add SpeechService.java Add VoiceRecorder.java Add activity_aidialog_sample.xml Add activity_speech.xml

---
+++ app/src/main/java/vivacity/com/br/testarea/AIDialogSampleActivity.java
@@ -0,0 +1,13 @@
+package vivacity.com.br.testarea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AIDialogSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aidialog_sample);
+    }
+}
